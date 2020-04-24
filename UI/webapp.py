94c4e1854cb614840b0869c9ea2455454a0334d9
Flavio Priotti <flaviojpriotti@gmail.com>
2020-04-23 21:13:27 -0300

    UI: Upload certificates from the UI and other changes.

--- UI/webapp.py
+++ UI/webapp.py
@@ -24,6 +24,7 @@ workspace = os.path.join(config['workspace']['path'], '')
 dirname = os.path.dirname(__file__)
 file_input = os.path.join(dirname, 'tmp', '')
 
+aws_folder = workspace + "AWS/"
 driverbehavior_folder = workspace + "DriverBehavior/"
 actionrecognition_folder = workspace + "ActionRecognition/"
 
@@ -119,11 +120,12 @@ def run_driver_management():
                           driverbehavior_folder + " && while true; do ros2 bag play truck.bag; done;") if (json['rosbag'] == "1") else ("")
 
         # Driver Actions Command
-        command_driver_actions = "source " + ROS_SOURCE + " && source " + driverbehavior_folder + "ets_ros2/install/setup.bash && source " + OPENVINO_SOURCE + \
-            " && cd "+ actionrecognition_folder + " && python3 action_recognition.py -m_en models/FP32/driver-action-recognition-adas-0002-encoder.xml -m_de models/FP32/driver-action-recognition-adas-0002-decoder.xml -lb driver_actions.txt -l /opt/intel/openvino/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU"
+        command_driver_actions = "source " + ROS_SOURCE + " && source " + OPENVINO_SOURCE + \
+            " && cd " + actionrecognition_folder + " && python3 action_recognition.py -m_en models/FP32/driver-action-recognition-adas-0002-encoder.xml -m_de models/FP32/driver-action-recognition-adas-0002-decoder.xml -lb driver_actions.txt -l /opt/intel/openvino/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU"
 
         if (json['camera_actions'] == "0"):
-            command_driver_actions += " -i '" + file_input + json['file_actions'] + "'"
+            command_driver_actions += " -i '" + \
+                file_input + json['file_actions'] + "'"
         else:
             if (json['camera'] == "0"):
                 command_driver_actions += " -i /dev/video1"
@@ -131,14 +133,17 @@ def run_driver_management():
                 command_driver_actions += " -i /dev/video0"
 
         if (json['aws_actions']):
-            command_driver_actions += " -e a1572pdc8tbdas-ats.iot.us-east-1.amazonaws.com -r " + workspace + "AWS/AmazonRootCA1.pem -c " + workspace + "AWS/a81867df13-certificate.pem.crt -k " + workspace + "AWS/a81867df13-private.pem.key -t actions/"
+            command_driver_actions += " -e a1572pdc8tbdas-ats.iot.us-east-1.amazonaws.com -r " + aws_folder + "AmazonRootCA1.pem -c " + \
+                aws_folder + "a81867df13-certificate.pem.crt -k " + \
+                aws_folder + "a81867df13-private.pem.key -t actions/"
 
         # Driver Behaviour Command
         command_driver_behaviour = "source " + ROS_SOURCE + " && source " + driverbehavior_folder + "ets_ros2/install/setup.bash && source " + OPENVINO_SOURCE + " && source " + driverbehavior_folder + "scripts/setupenv.sh && cd " + driverbehavior_folder + "build/intel64/Release && ./driver_behavior -d " + \
             json['target'] + " -d_hp " + json['target_hp']
 
         if (json['camera'] == "0"):
-            command_driver_behaviour += " -i '" + file_input + json['file'] + "'"
+            command_driver_behaviour += " -i '" + \
+                file_input + json['file'] + "'"
         else:
             command_driver_behaviour += " -i cam"
 
@@ -157,7 +162,8 @@ def run_driver_management():
         command_driver_behaviour += " -l /opt/intel/openvino/inference_engine/lib/intel64/libcpu_extension_sse4.so "
         # Recognition of the Driver
         if (json['recognition'] == "1"):
-            command_driver_behaviour += " -d_recognition -fg " + driverbehavior_folder + "scripts/faces_gallery.json"
+            command_driver_behaviour += " -d_recognition -fg " + \
+                driverbehavior_folder + "scripts/faces_gallery.json"
         # Landmarks Detection
         if (json['landmarks'] == "1"):
             command_driver_behaviour += " -dlib_lm"
@@ -221,10 +227,12 @@ def create_driver_management():
         if request.files:
             file = request.files["file"]
             # Save the file with the Driver's name and add the same extension
-            file.save(os.path.join(driverbehavior_folder + "drivers/", request.values['driver'] + "." + file.filename.split('.')[-1]))
+            file.save(os.path.join(driverbehavior_folder + "drivers/",
+                                   request.values['driver'] + "." + file.filename.split('.')[-1]))
             # Generating the list with all the drivers
             print("Creating New Driver")
-            shell_communication("cd " + driverbehavior_folder + "scripts/ && python3 create_list.py ../drivers/")
+            shell_communication("cd " + driverbehavior_folder +
+                                "scripts/ && python3 create_list.py ../drivers/")
             out = "New driver created!"
         return jsonify(out=out)
 
@@ -270,6 +278,36 @@ def certificates():
         'workspace': workspace
     }
     return render_template("certificates.html", **templateData)
+
+
+@app.route('/upload_certificates', methods=['POST', 'GET'])
+# Upload the video file selected into a temporal folder (Video Upload Folder in configuration)
+def upload_certificates():
+    out = False
+    if request.method == 'POST':
+        if request.files:
+            # Certificate
+            certificate = request.files.get('certificate', False)
+            if certificate:
+                file = request.files["certificate"]
+                file.save(os.path.join(aws_folder, file.filename))
+            # Private Key
+            private_key = request.files.get('private_key', False)
+            if private_key:
+                file = request.files["private_key"]
+                file.save(os.path.join(aws_folder, file.filename))
+            # Public Key
+            public_key = request.files.get('public_key', False)
+            if public_key:
+                file = request.files["public_key"]
+                file.save(os.path.join(aws_folder, file.filename))
+            # RootCA
+            root_ca = request.files.get('root_ca', False)
+            if root_ca:
+                file = request.files["root_ca"]
+                file.save(os.path.join(aws_folder, file.filename))
+            out = True
+    return jsonify(out=out)
 
 
 @app.route('/check_pass', methods=['POST', 'GET'])
